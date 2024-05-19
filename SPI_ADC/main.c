95498a450affd7aa0a14425f1da59d15e7fc99e3
Le Thien Son <lethienson@Les-MacBook-Pro-7.local>
2024-05-19 10:16:05 +0700

    Completed UART Ex1

--- SPI_ADC/main.c
+++ SPI_ADC/main.c
@@ -1,23 +1,104 @@
 #include <stdio.h>
 #include "NUC100Series.h"
+#include "SYS.h"
+#include "ADC.h"
+#include "SPI.h"
 
-int main(void){
-	PC->PMD &= (~(0b11<<24));
-	PC->PMD |= (0b01 << 24);
-	PC->PMD &= (~(0b11<<30));
-	PC->PMD |= (0b01 << 30);
-	
-	
-	PB->PMD &= (~(0x03 << 30));
-	 
-	while(1){
-		if(!(PB->PIN &(1<<15))){
-			PC->DOUT &= ~(1<<12);
-			PC->DOUT |= (1 << 15);
-		} else {
-			PC->DOUT |= (1<<12);
-			PC->DOUT &= ~ (1 << 15);
-		}
-	}
-	
+#define TEAM_NAME "Team 8"
+
+void SYS_Init(void);
+void ADC_Init(void);
+void SPI2_Init(void);
+void send_SPI2(const char *data);
+
+int main(void) {
+    uint32_t adc_value;
+    float voltage;
+
+    SYS_Init();
+    ADC_Init();
+    SPI2_Init();
+
+    ADC->ADCR |= (1 << 11); // Start ADC channel 7 conversion
+
+    while (1) {
+        while (!(ADC->ADSR & (1 << 0))); // Wait for conversion to complete (ADF=1)
+        ADC->ADSR |= (1 << 0); // Write 1 to clear ADF
+
+        adc_value = ADC->ADDR[7] & 0xFFF; // Read ADC result
+        voltage = (adc_value * 3.3f) / 4095.0f;
+
+        if (voltage > 2.0f) {
+            send_SPI2(TEAM_NAME);
+        }
+    }
+}
+
+void SYS_Init(void) {
+    SYS_UnlockReg();
+
+    CLK->PWRCON |= (1 << 0); // Enable external 12 MHz XTAL
+    while (!(CLK->CLKSTATUS & (1 << 0))); // Wait for 12 MHz clock ready
+
+    CLK->PLLCON &= ~(1 << 19); // PLL input is HXT
+    CLK->PLLCON &= ~(1 << 16); // PLL in normal mode
+    CLK->PLLCON &= ~(0x01FF << 0);
+    CLK->PLLCON |= 48;
+    CLK->PLLCON &= ~(1 << 18); // Enable PLLOUT
+    while (!(CLK->CLKSTATUS & (1 << 2))); // Wait for PLL ready
+
+    CLK->CLKSEL0 &= ~(0x07 << 0); // Switch HCLK clock source to HXT
+    CLK->CLKSEL0 |= (0x02 << 0);
+
+    CLK->CLKDIV &= ~(0x0F << 0); // Set HCLK divider to 1
+
+    CLK->APBCLK |= (1 << 28); // Enable ADC clock
+    CLK->CLKSEL1 &= ~(0x03 << 2); // ADC clock source is 12 MHz
+    CLK->CLKDIV &= ~(0x0FF << 16);
+    CLK->CLKDIV |= (0x0B << 16); // ADC clock divider is (11+1) --> 1 MHz
+
+    CLK->APBCLK |= (1 << 14); // Enable SPI2 clock
+    CLK->CLKDIV1 &= ~(0x0F << 20);
+    CLK->CLKDIV1 |= (0x0B << 20); // SPI2 clock divider is (11+1) --> 1 MHz
+
+    SystemCoreClockUpdate();
+    SYS_LockReg();
+}
+
+void ADC_Init(void) {
+    PA->PMD &= ~(0b11 << 14); // Clear PA.7 mode
+    PA->PMD |= (0b00 << 14);  // Set PA.7 to input mode
+    PA->OFFD |= (1 << 7); // PA.7 digital input path disabled
+    SYS->GPA_MFP |= (1 << 7); // GPA_MFP[7] = 1 for ADC7
+    SYS->ALT_MFP &= ~(1 << 11); // ALT_MFP[11] = 0 for ADC7
+
+    ADC->ADCR |= (0x03 << 2); // Continuous scan mode
+    ADC->ADCR &= ~(1 << 1); // ADC interrupt is disabled
+    ADC->ADCR |= (1 << 0); // ADC is enabled
+    ADC->ADCHER &= ~(0x03 << 8); // ADC7 input source is external pin
+    ADC->ADCHER |= (1 << 7); // Enable ADC channel 7
+}
+
+void SPI2_Init(void) {
+    SYS->GPD_MFP |= (1 << 0) | (1 << 1) | (1 << 3); // Configure SPI2 pins
+
+    SPI2->CNTRL &= ~(1 << 23); // Disable variable clock feature
+    SPI2->CNTRL &= ~(1 << 22); // Disable two bits transfer mode
+    SPI2->CNTRL &= ~(1 << 18); // Select Master mode
+    SPI2->CNTRL &= ~(1 << 17); // Disable SPI interrupt
+    SPI2->CNTRL |= (1 << 11); // SPI clock idle high
+    SPI2->CNTRL &= ~(1 << 10); // MSB sent first
+    SPI2->CNTRL &= ~(3 << 8); // One transmit/receive word in one data transfer
+    SPI2->CNTRL &= ~(31 << 3); // Transmit/Receive bit length
+    SPI2->CNTRL |= (7 << 3); // 8 bits transmitted/received per data transfer
+    SPI2->CNTRL |= (1 << 2); // Transmit at negative edge of SPI CLK
+    SPI2->DIVIDER = 11; // SPI clock divider. SPI clock = HCLK / ((DIVIDER+1)*2). HCLK = 50 MHz
+}
+
+void send_SPI2(const char *data) {
+    while (*data) {
+        SPI2->TX[0] = *data++;
+        SPI2->CNTRL |= (1 << 0); // Start transfer
+        while (SPI2->CNTRL & (1 << 0)); // Wait for transfer to complete
+    }
 }
--- UART/main.c
+++ UART/main.c
@@ -1,104 +1,102 @@
-#include <stdio.h>
-#include "NUC100Series.h"
-#include "SYS.h"
-#include "ADC.h"
-#include "SPI.h"
-
-#define TEAM_NAME "Team 8"
-
-void SYS_Init(void);
-void ADC_Init(void);
-void SPI2_Init(void);
-void send_SPI2(const char *data);
-
-int main(void) {
-    uint32_t adc_value;
-    float voltage;
-
-    SYS_Init();
-    ADC_Init();
-    SPI2_Init();
-
-    ADC->ADCR |= (1 << 11); // Start ADC channel 7 conversion
-
-    while (1) {
-        while (!(ADC->ADSR & (1 << 0))); // Wait for conversion to complete (ADF=1)
-        ADC->ADSR |= (1 << 0); // Write 1 to clear ADF
-
-        adc_value = ADC->ADDR[7] & 0xFFF; // Read ADC result
-        voltage = (adc_value * 3.3f) / 4095.0f;
-
-        if (voltage > 2.0f) {
-            send_SPI2(TEAM_NAME);
-        }
-    }
-}
-
-void SYS_Init(void) {
-    SYS_UnlockReg();
-
-    CLK->PWRCON |= (1 << 0); // Enable external 12 MHz XTAL
-    while (!(CLK->CLKSTATUS & (1 << 0))); // Wait for 12 MHz clock ready
-
-    CLK->PLLCON &= ~(1 << 19); // PLL input is HXT
-    CLK->PLLCON &= ~(1 << 16); // PLL in normal mode
-    CLK->PLLCON &= ~(0x01FF << 0);
-    CLK->PLLCON |= 48;
-    CLK->PLLCON &= ~(1 << 18); // Enable PLLOUT
-    while (!(CLK->CLKSTATUS & (1 << 2))); // Wait for PLL ready
-
-    CLK->CLKSEL0 &= ~(0x07 << 0); // Switch HCLK clock source to HXT
-    CLK->CLKSEL0 |= (0x02 << 0);
-
-    CLK->CLKDIV &= ~(0x0F << 0); // Set HCLK divider to 1
-
-    CLK->APBCLK |= (1 << 28); // Enable ADC clock
-    CLK->CLKSEL1 &= ~(0x03 << 2); // ADC clock source is 12 MHz
-    CLK->CLKDIV &= ~(0x0FF << 16);
-    CLK->CLKDIV |= (0x0B << 16); // ADC clock divider is (11+1) --> 1 MHz
-
-    CLK->APBCLK |= (1 << 14); // Enable SPI2 clock
-    CLK->CLKDIV1 &= ~(0x0F << 20);
-    CLK->CLKDIV1 |= (0x0B << 20); // SPI2 clock divider is (11+1) --> 1 MHz
-
-    SystemCoreClockUpdate();
-    SYS_LockReg();
-}
-
-void ADC_Init(void) {
-    PA->PMD &= ~(0b11 << 14); // Clear PA.7 mode
-    PA->PMD |= (0b00 << 14);  // Set PA.7 to input mode
-    PA->OFFD |= (1 << 7); // PA.7 digital input path disabled
-    SYS->GPA_MFP |= (1 << 7); // GPA_MFP[7] = 1 for ADC7
-    SYS->ALT_MFP &= ~(1 << 11); // ALT_MFP[11] = 0 for ADC7
-
-    ADC->ADCR |= (0x03 << 2); // Continuous scan mode
-    ADC->ADCR &= ~(1 << 1); // ADC interrupt is disabled
-    ADC->ADCR |= (1 << 0); // ADC is enabled
-    ADC->ADCHER &= ~(0x03 << 8); // ADC7 input source is external pin
-    ADC->ADCHER |= (1 << 7); // Enable ADC channel 7
-}
-
-void SPI2_Init(void) {
-    SYS->GPD_MFP |= (1 << 0) | (1 << 1) | (1 << 3); // Configure SPI2 pins
-
-    SPI2->CNTRL &= ~(1 << 23); // Disable variable clock feature
-    SPI2->CNTRL &= ~(1 << 22); // Disable two bits transfer mode
-    SPI2->CNTRL &= ~(1 << 18); // Select Master mode
-    SPI2->CNTRL &= ~(1 << 17); // Disable SPI interrupt
-    SPI2->CNTRL |= (1 << 11); // SPI clock idle high
-    SPI2->CNTRL &= ~(1 << 10); // MSB sent first
-    SPI2->CNTRL &= ~(3 << 8); // One transmit/receive word in one data transfer
-    SPI2->CNTRL &= ~(31 << 3); // Transmit/Receive bit length
-    SPI2->CNTRL |= (7 << 3); // 8 bits transmitted/received per data transfer
-    SPI2->CNTRL |= (1 << 2); // Transmit at negative edge of SPI CLK
-    SPI2->DIVIDER = 11; // SPI clock divider. SPI clock = HCLK / ((DIVIDER+1)*2). HCLK = 50 MHz
-}
-
-void send_SPI2(const char *data) {
-    while (*data) {
-        SPI2->TX[0] = *data++;
-        SPI2->CNTRL |= (1 << 0); // Start transfer
-        while (SPI2->CNTRL & (1 << 0)); // Wait for transfer to complete
-    }
-}
+//------------------------------------------- main.c CODE STARTS ---------------------------------------------------------------------------
+#include <stdio.h>
+#include "NUC100Series.h"
+
+void System_Config(void);
+void UART0_Config(void);
+void UART0_SendChar(int ch);
+char UART0_GetChar(void);
+void Interrupt_Config(void);
+void UART02_IRQHandler(void);
+
+int main(void)
+{
+	System_Config();
+	UART0_Config();
+	Interrupt_Config();
+	GPIO_SetMode(PC,BIT12,GPIO_MODE_OUTPUT);
+	while(1){
+	}
+}
+
+void System_Config (void){
+	SYS_UnlockReg(); // Unlock protected registers
+
+	// enable clock sources
+	CLK->PWRCON |= (0x01 << 0);
+	while(!(CLK->CLKSTATUS & (1 << 0)));
+
+	//PLL configuration starts
+	CLK->PLLCON &= ~(1<<19); //0: PLL input is HXT
+	CLK->PLLCON &= ~(1<<16); //PLL in normal mode
+	CLK->PLLCON &= (~(0x01FF << 0));
+	CLK->PLLCON |= 48;
+	CLK->PLLCON &= ~(1<<18); //0: enable PLLOUT
+	while(!(CLK->CLKSTATUS & (0x01 << 2)));
+	//PLL configuration ends
+
+	// CPU clock source selection
+	CLK->CLKSEL0 &= (~(0x07 << 0));
+	CLK->CLKSEL0 |= (0x02 << 0);
+	//clock frequency division
+	CLK->CLKDIV &= (~0x0F << 0);
+
+	//UART0 Clock selection and configuration
+	CLK->CLKSEL1 |= (0x03 << 24); // UART0 clock source is 22.1184 MHz
+	CLK->CLKDIV &= ~(0x0F << 8); // clock divider is 1
+	CLK->APBCLK |= (0x01 << 16); // enable UART0 clock
+
+	SYS_LockReg();  // Lock protected registers
+}
+
+void UART0_Config(void) {
+	// UART0 pin configuration. PB.1 pin is for UART0 TX
+	PB->PMD &= ~(0x03 << 2);
+	PB->PMD |= (0x01 << 2); // PB.1 is output pin
+	SYS->GPB_MFP |= (0x01 << 1); // GPB_MFP[1] = 1 -> PB.1 is UART0 TX pin
+	SYS->GPB_MFP |= (0x01 << 0); // GPB_MFP[0] = 1 -> PB.0 is UART0 RX pin
+
+	// UART0 operation configuration
+	UART0->FCR |= (0x03 << 1); // clear both TX & RX FIFO
+	UART0->FCR &= ~(0x0F << 16); // FIFO Trigger Level is 1 byte
+	UART0->LCR &= ~(0x01 << 3); // no parity bit
+	UART0->LCR &= ~(0x01 << 2); // one stop bit
+	UART0->LCR |= (0x03 << 0); // 8 data bit
+
+	//Baud rate config: BRD/A = 1, DIV_X_EN=0
+	//--> Mode 0, Baud rate = UART_CLK/[16*(A+2)] = 22.1184 MHz/[16*(10+2)]= 115200 bps
+	UART0->BAUD &= ~(0x0FFFF << 0);
+	UART0->BAUD |= 10;
+	UART0->BAUD &= ~(0x03 << 28); // mode 0
+}
+
+void UART0_SendChar(int ch){
+	while(UART0->FSR & (0x01 << 23)); //wait until TX FIFO is not full
+	UART0->DATA = ch;
+	if(ch == '\n'){
+		while(UART0->FSR & (0x01 << 23));
+		UART0->DATA = '\r';
+	}
+}
+
+char UART0_GetChar(void){
+	while(1){
+		if(!(UART0->FSR & (0x01 << 14))){
+			return(UART0->DATA);
+		}
+	}
+}
+
+
+void Interrupt_Config(void){
+	UART0->IER |= (0b11 << 0); //Set UA_IER bit 1:0 to 1 to enable RDA and IEN
+	NVIC->ISER[0] = 1<<12;	//Enable UART02 Interrupt
+	NVIC->IP[3] &= (~(3 << 6));  //Set highest interrupt priority for UART02 Interrupt
+}
+
+void UART02_IRQHandler(void)
+{
+	UART0_SendChar(UART0_GetChar());
+	PC->DOUT ^= (1 << 12);
+}
+//------------------------------------------- main.c CODE ENDS ---------------------------------------------------------------------------
